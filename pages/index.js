c70b2f63b215153fc09709beedf393239ecbf2b7
Yannick <musayann@octangrp.com>
2020-08-17 09:46:17 +0200

    Integrate firabse

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import Head from "next/head";
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import styles from "../styles/Home.module.css";
+import firebase from "../lib/firebase";
 
 export default class Home extends Component {
   componentDidMount() {
-    console.log(process.env.GREETING);
+    console.log(process.env.FIREBASE_API_KEY);
+    const config = {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    }
+    console.log(config);
+    // firebase.database().ref('events').update({
+    //   new_neaaa: {
+    //     date_of_birth: "December 9, 1906",
+    //     full_name: "Grace Hopper",
+    //   },
+    // });
   }
   render() {
     return (
