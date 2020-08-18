0b30d0475eb8fcb04f2da8847778ab9097f3705e
Yannick <musayann@octangrp.com>
2020-08-18 16:49:04 +0200

    Pages playaround

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,16 @@ import Head from "next/head";
 import React, { Component } from "react";
 import styles from "../styles/Home.module.css";
 import firebase from "../lib/firebase";
+import { connect } from 'react-redux'
 
-export default class Home extends Component {
+class Home extends Component {
   componentDidMount() {
-    console.log(process.env.FIREBASE_API_KEY);
-    const config = {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    }
-    console.log(config);
-    // firebase.database().ref('events').update({
-    //   new_neaaa: {
-    //     date_of_birth: "December 9, 1906",
-    //     full_name: "Grace Hopper",
-    //   },
-    // });
+    firebase.database().ref('events').update({
+      new_neaaa: {
+        date_of_birth: "December 9, 1906",
+        full_name: "Grace Hopper",
+      },
+    });
   }
   render() {
     return (
@@ -87,3 +76,11 @@ export default class Home extends Component {
     );
   }
 }
+
+export const mapStateToProps = (state) => ({
+  lastUpdate: state.lastUpdate,
+  light: state.light,
+  count: state.count,
+});
+
+export default connect(mapStateToProps)(Home);
